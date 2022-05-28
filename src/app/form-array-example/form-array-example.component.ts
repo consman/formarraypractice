c63e8bc6fe57cbcad9d32fcf66ffcd87b172c313
bob <bob.dev.oak@gmail.com>
2022-05-27 22:31:10 -0700

    Improve logic to handle existing data to be loaded into the form array of lessons, and therefore onto the page when it loads.

--- src/app/form-array-example/form-array-example.component.ts
+++ src/app/form-array-example/form-array-example.component.ts
@@ -8,27 +8,42 @@ import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angul
 })
 export class FormArrayExampleComponent {
 
+  lessonData = [{'title':'First Title','level':'advanced'},
+    {'title':'Second Title','level':'intermediate'}];
+
   form = this.fb.group({
     title : new FormControl('My Title'),
     level : new FormControl('Level 1'),
     lessons: this.fb.array([])
   });
 
-constructor(private fb:FormBuilder) {}
+  empty = '';
 
-get lessons() {
-  return this.form.controls["lessons"] as FormArray;
-}
+  constructor(private fb:FormBuilder) {}
 
-addLesson() {
-  const lessonForm = this.fb.group({
-    title: ['', Validators.required],
-    level: ['beginner', Validators.required]
-  });
-  this.lessons.push(lessonForm);
-}
+  ngOnInit(){
+    this.lessonData.forEach(ld => {
+      const lform = this.fb.group({
+        title: new FormControl(ld.title),
+        level : new FormControl(ld.level),
+      });
+      this.lessons.push(lform);
+    });
+  }
 
-deleteLesson(lessonIndex: number) {
-  this.lessons.removeAt(lessonIndex);
-}
+  get lessons() {
+    return this.form.controls["lessons"] as FormArray;
+  }
+
+  addLesson() {
+    const lessonForm = this.fb.group({
+      title: ['', Validators.required],
+      level: ['beginner', Validators.required]
+    });
+    this.lessons.push(lessonForm);
+  }
+
+  deleteLesson(lessonIndex: number) {
+    this.lessons.removeAt(lessonIndex);
+  }
 }
